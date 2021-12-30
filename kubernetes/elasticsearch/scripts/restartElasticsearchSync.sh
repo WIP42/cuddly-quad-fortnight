bc16af080dfc88b6275f45492583e4a909727cc9
WIP42 <pvanderr@gmail.com>
2021-12-30 12:18:31 +0100

    Better killing

--- kubernetes/elasticsearch/scripts/restartElasticsearchSync.sh
+++ kubernetes/elasticsearch/scripts/restartElasticsearchSync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for POD in `kubectl get pods|grep elasticsearch|awk '{print $1}'`;do
-  kubectl port-forward $POD 9200:9200 &
+for POD in $(kubectl get pods|grep elasticsearch|awk '{print $1}');do
+  kubectl port-forward "$POD" 9200:9200 &
   #sleep 20;
   echo "Please <<<WAIT FOR>>> 'Forwarding from...' message and <<<PRESS ENTER>>> to continue with forwarded ports connected to pod: '$POD'";echo '';
   read a;
--- kubernetes/elasticsearch/scripts/stopElasticsearchSyncAndKillPod.sh
+++ kubernetes/elasticsearch/scripts/stopElasticsearchSyncAndKillPod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for POD in `kubectl get pods|grep elasticsearch|awk '{print $1}'`;do
-  kubectl port-forward $POD 9200:9200 &
+for POD in $(kubectl get pods|grep elasticsearch|awk '{print $1}');do
+  kubectl port-forward "$POD" 9200:9200 &
   #sleep 20;
   echo "Please <<<WAIT FOR>>> 'Forwarding from...' message and <<<PRESS ENTER>>> to continue with forwarded ports connected to pod: '$POD'";echo '';
   read a;
@@ -20,6 +20,6 @@ for POD in `kubectl get pods|grep elasticsearch|awk '{print $1}'`;do
 
   echo "Sleeping for 60 seconds before killing pod: '$POD'";echo '';
   sleep 60;
-  kubectl delete pod $POD;
+  kubectl delete pod "$POD";
 done
 
